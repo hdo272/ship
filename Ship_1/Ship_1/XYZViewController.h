be790cf01b8a4e2a8f87e98ba56c8f6a5e381214
hdo272 <hillaryjiado@gmail.com>
2014-10-31 23:41:10 -0400

    this time?

---
+++ Ship_1/Ship_1/XYZViewController.h
@@ -0,0 +1,13 @@
+//
+//  XYZViewController.h
+//  Ship_1
+//
+//  Created by Hillary Do on 10/31/14.
+//  Copyright (c) 2014 ___SHIP___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYZViewController : UIViewController
+
+@end
---
+++ Ship_1/Ship_1/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Ship_1
+//
+//  Created by Hillary Do on 10/31/14.
+//  Copyright (c) 2014 ___SHIP___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "XYZAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([XYZAppDelegate class]));
+    }
+}
