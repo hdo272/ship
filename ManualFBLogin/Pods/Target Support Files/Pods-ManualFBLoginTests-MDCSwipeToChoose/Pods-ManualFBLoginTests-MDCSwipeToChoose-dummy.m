bd11b5af37b7f69ff32fd5dc406460a676140291
theannielin <annielin@college.harvard.edu>
2014-11-01 20:10:11 -0400

    wuttt

---
+++ ManualFBLogin/Pods/Target Support Files/Pods-ManualFBLoginTests-MDCSwipeToChoose/Pods-ManualFBLoginTests-MDCSwipeToChoose-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ManualFBLoginTests_MDCSwipeToChoose : NSObject
+@end
+@implementation PodsDummy_Pods_ManualFBLoginTests_MDCSwipeToChoose
+@end
